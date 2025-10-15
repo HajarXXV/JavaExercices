4931c7097261892f64043aeaaa4bd8c7f6e88c9d
HajarXXV <hajarhadidou@gmail.com>
2025-10-15 11:38:16 +0100

    LambdaExercice-reference

--- src/LamdaExpression/Main.java
+++ src/LamdaExpression/Main.java
@@ -9,21 +9,21 @@ public class Main {
 
     static void main() {
 //        Exercice1
-//        Function<String, Integer> longueur = s -> s.length();
+//        Function<String, Integer> longueur = String::length;
 //        String chaine = "JavaLambda";
 //        System.out.println("La longueur du chaine est " + longueur.apply(chaine));
 
 //        Exercice2
 //        List<String> noms = Arrays.asList("Khadija", "Hiba", "Mohammed");
-//        Consumer<String> afficher = nom -> System.out.println(nom);
+//        Consumer<String> afficher = System.out::println;
 //        noms.forEach(afficher);
 
 //        Exercice3
-//        Supplier<Double> nombre = () -> Math.random()*10;
+//        Supplier<Double> nombre = Math::random;
 //        System.out.println(nombre.get());
 
 //        Exercice4
-//        Function<String, Personne> constructeur = nom -> new Personne(nom);
+//        Function<String, Personne> constructeur = Personne::new;
 //        constructeur.apply("Hajar");
     }
 }
