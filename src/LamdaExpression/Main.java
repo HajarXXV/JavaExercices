bc33c41b986d820d567700c272152c559c18f402
HajarXXV <hajarhadidou@gmail.com>
2025-10-15 10:01:13 +0100

    LambdaExercice4

--- src/LamdaExpression/Main.java
+++ src/LamdaExpression/Main.java
@@ -3,22 +3,28 @@ import java.util.function.Function;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class Main {
 
     static void main() {
 //        Exercice1
-//       Function<String, Integer> longueur = s -> s.length();
-//       String chaine = "JavaLambda";
+//        Function<String, Integer> longueur = s -> s.length();
+//        String chaine = "JavaLambda";
 //        System.out.println("La longueur du chaine est " + longueur.apply(chaine));
 
 //        Exercice2
-        List<String> noms = Arrays.asList("Khadija", "Hiba", "Mohammed");
-        Consumer<String> afficher = nom -> System.out.println(nom);
-//        for(String nom : noms) {
-//            afficher.accept(nom);
-//        }
-        noms.forEach(afficher);
+//        List<String> noms = Arrays.asList("Khadija", "Hiba", "Mohammed");
+//        Consumer<String> afficher = nom -> System.out.println(nom);
+//        noms.forEach(afficher);
+
+//        Exercice3
+//        Supplier<Double> nombre = () -> Math.random()*10;
+//        System.out.println(nombre.get());
+
+//        Exercice4
+//        Function<String, Personne> constructeur = nom -> new Personne(nom);
+//        constructeur.apply("Hajar");
     }
 }
 
---
+++ src/LamdaExpression/Personne.java
@@ -0,0 +1,11 @@
+package LamdaExpression;
+
+public class Personne {
+    private String nom;
+
+    //constructor
+    public Personne(String nom) {
+        this.nom = nom;
+        System.out.println("Nouvelle personne crée: " + nom);
+    }
+}
